f654de99ee706c29a2ae11d94021be324d551900
mck-9061 <32172648+mck-9061@users.noreply.github.com>
2025-04-16 18:26:47 +0100

    Refactor

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def distance(v1, v2):
 
 colour_bias = 1.0
 
+
 def sort_nearest_neighbour(array):
     # Sort an array of type [tuple[tuple[int, int, int], int, int]] by the nearest neighbour
     remaining = [a for a in array]
@@ -38,8 +39,9 @@ def sort_nearest_neighbour(array):
 
         for a in remaining:
             # print(a)
-            xyrgb = (a[1], a[2], a[0][0]*colour_bias, a[0][1]*colour_bias, a[0][2]*colour_bias)
-            currentXyrgb = (current[1], current[2], current[0][0]*colour_bias, current[0][1]*colour_bias, current[0][2]*colour_bias)
+            xyrgb = (a[1], a[2], a[0][0] * colour_bias, a[0][1] * colour_bias, a[0][2] * colour_bias)
+            currentXyrgb = (current[1], current[2], current[0][0] * colour_bias, current[0][1] * colour_bias,
+                            current[0][2] * colour_bias)
 
             d = distance_5d(xyrgb, currentXyrgb)
             if d < nearestDistance:
@@ -53,28 +55,34 @@ def sort_nearest_neighbour(array):
     return done
 
 
-pre_defined_colours = {
-    "blank": (0, 0, 0, []),
-    "red": (232, 24, 32, []),
-    "orange": (240, 144, 24, []),
-    "yellow": (248, 240, 0, []),
-    "light_green": (136, 192, 56, []),
-    "green": (56, 176, 72, []),
-    "light_blue": (0, 168, 232, []),
-    "blue": (0, 80, 160, []),
-    "purple": (96, 40, 144, []),
-    "pink": (232, 0, 136, []),
-    "dark_red": (152, 8, 8, []),
-    "gold": (128, 120, 0, []),
-    "dark_green": (0, 88, 32, []),
-    "dark_blue": (0, 48, 96, []),
-    "white": (248, 248, 248, []),
-    "grey": (128, 128, 128, []),
-    "black": (0, 0, 0, [])
-}
+pre_defined_colours = {}
+
+
+def initialise_colours():
+    global pre_defined_colours
+    pre_defined_colours = {
+        "red": (232, 24, 32, []),
+        "orange": (240, 144, 24, []),
+        "yellow": (248, 240, 0, []),
+        "light_green": (136, 192, 56, []),
+        "green": (56, 176, 72, []),
+        "light_blue": (0, 168, 232, []),
+        "blue": (0, 80, 160, []),
+        "purple": (96, 40, 144, []),
+        "pink": (232, 0, 136, []),
+        "dark_red": (152, 8, 8, []),
+        "gold": (128, 120, 0, []),
+        "dark_green": (0, 88, 32, []),
+        "dark_blue": (0, 48, 96, []),
+        "white": (248, 248, 248, []),
+        "grey": (128, 128, 128, []),
+        "black": (0, 0, 0, [])
+    }
 
 
 def build_pixels():
+    global pre_defined_colours
+
     im = Image.open("in.png")
     im.thumbnail((62, 62), Image.Resampling.LANCZOS)
     out = Image.new("RGBA", (62, 62), 0xffffff)
@@ -85,12 +93,10 @@ def build_pixels():
             r, g, b, a = im.getpixel((x, y))
             pixel = (r, g, b)
 
-            closest_match = (0, 0, 0)
             closest_colour = "black"
             closest_distance = 99999
 
             if a == 0:
-                pre_defined_colours["blank"][3].append((pixel, x, y))
                 continue
 
             for c_name in pre_defined_colours:
@@ -98,7 +104,6 @@ def build_pixels():
                 d = distance(c, pixel)
                 if d <= closest_distance:
                     closest_distance = d
-                    closest_match = c
                     closest_colour = c_name
 
             pre_defined_colours[closest_colour][3].append((pixel, x, y))
@@ -106,16 +111,13 @@ def build_pixels():
     x = 0
     y = 0
     for c_name in pre_defined_colours:
-        #print(c_name)
-
-        if not c_name == "blank":
-            pre_defined_colours[c_name] = (pre_defined_colours[c_name][0], pre_defined_colours[c_name][1], pre_defined_colours[c_name][2], sort_nearest_neighbour(pre_defined_colours[c_name][3]))
+        pre_defined_colours[c_name] = (
+            pre_defined_colours[c_name][0], pre_defined_colours[c_name][1], pre_defined_colours[c_name][2],
+            sort_nearest_neighbour(pre_defined_colours[c_name][3]))
 
         for pixel in pre_defined_colours[c_name][3]:
             # print(pixel)
             toWrite = (pixel[0][0], pixel[0][1], pixel[0][2], 255)
-            if c_name == "blank":
-                toWrite = (pixel[0][0], pixel[0][1], pixel[0][2], 0)
 
             out.putpixel((pixel[1], pixel[2]), toWrite)
             x += 1
@@ -126,12 +128,69 @@ def build_pixels():
     out.save("out.png")
 
 
+def move_cursor(current, target, outFile):
+    while target[0] != current[0]:
+        if target[0] < current[0]:
+            outFile.write("left\n")
+            current[0] -= 1
+        else:
+            outFile.write("right\n")
+            current[0] += 1
+
+    while target[1] != current[1]:
+        if target[1] < current[1]:
+            outFile.write("up\n")
+            current[1] -= 1
+        else:
+            outFile.write("down\n")
+            current[1] += 1
+
+
+# rgb param: 0 = red, 1 = green, 2 = blue
+def move_rgb_slider(colour_cursor, rgb, current, target, outFile):
+    if current[rgb] != target[rgb]:
+        while colour_cursor[2] != 0:
+            outFile.write("cup\n")
+            colour_cursor[2] -= 1
+
+        while colour_cursor[0] != rgb:
+
+            if colour_cursor[0] > rgb:
+                outFile.write("cdown\n")
+                colour_cursor[0] -= 1
+            else:
+                outFile.write("cup\n")
+                colour_cursor[0] += 1
+
+        j = 1
+        while current[rgb] > target[rgb]:
+            outFile.write("cleft\n")
+            current[rgb] -= 1
+            j += 1
+            if j % 4 == 0:
+                j = 1
+                outFile.write("cup\n")
+                outFile.write("cdown\n")
+
+        j = 1
+        while current[rgb] < target[rgb]:
+            outFile.write("cright\n")
+            current[rgb] += 1
+            j += 1
+            if j % 4 == 0:
+                j = 1
+                outFile.write("cup\n")
+                outFile.write("cdown\n")
+
+
 def build_sequence():
+    global pre_defined_colours
+
     out = open("out.txt", "w+", encoding="utf-8")
 
     set_selected_colour = ["a", "x", "b", "right", "a", "a", "b", "left", "a"]
 
-    pixels_remaining = sum([len(pre_defined_colours[a][3]) for a in pre_defined_colours if a != "blank"])
+    pixels_remaining = sum([len(pre_defined_colours[a][3]) for a in pre_defined_colours])
     out.write("-remaining: " + str(pixels_remaining) + "\n")
 
     # Set up
@@ -149,41 +208,24 @@ def build_sequence():
 
     c_id = -1
     cursor = [0, 0]
-    current_rgb = [0, 0, 0]
-    # Index 0: 0 = R, 1 = G, 2 = B, 3 = Pre-defined section
+    # Index 0: 0 = red, 1 = green, 2 = blue, 3 = Pre-defined section
     colour_cursor = [3, 0, 0]
-    for c_name in pre_defined_colours:
-        #print(c_name)
-        if c_name == "blank":
-            continue
 
+    for c_name in pre_defined_colours:
         out.write("---------------" + c_name + "---------------\n")
 
         c_id += 1
 
-        if not len(pre_defined_colours[c_name][3]) == 0:
-            # Move to first pixel
-            pixel1 = pre_defined_colours[c_name][3][0]
-            x = pixel1[1]
-            y = pixel1[2]
+        if len(pre_defined_colours[c_name][3]) == 0:
+            continue
 
-            target_cursor = (x, y)
+        # Move to first pixel
+        pixel1 = pre_defined_colours[c_name][3][0]
+        x = pixel1[1]
+        y = pixel1[2]
 
-            while target_cursor[0] != cursor[0]:
-                if target_cursor[0] < cursor[0]:
-                    out.write("left\n")
-                    cursor[0] -= 1
-                else:
-                    out.write("right\n")
-                    cursor[0] += 1
-
-            while target_cursor[1] != cursor[1]:
-                if target_cursor[1] < cursor[1]:
-                    out.write("up\n")
-                    cursor[1] -= 1
-                else:
-                    out.write("down\n")
-                    cursor[1] += 1
+        target_cursor = (x, y)
+        move_cursor(cursor, target_cursor, out)
 
         # Move to pre-defined colour section
         while colour_cursor[0] != 3:
@@ -221,123 +263,14 @@ def build_sequence():
             x = pixel[1]
             y = pixel[2]
 
-            if rgb[0] != current_rgb[0]:
-                # Move red slider
-                while colour_cursor[0] != 0:
-                    out.write("cup\n")
-                    if colour_cursor[2] != 0:
-                        colour_cursor[2] -= 1
-                    else:
-                        colour_cursor[0] -= 1
-
-                j = 1
-                while current_rgb[0] > rgb[0]:
-                    out.write("cleft\n")
-                    current_rgb[0] -= 1
-                    j += 1
-                    if j % 5 == 0:
-                        j = 1
-                        out.write("cdown\n")
-                        out.write("cup\n")
-
-                j = 1
-                while current_rgb[0] < rgb[0]:
-                    out.write("cright\n")
-                    current_rgb[0] += 1
-                    j += 1
-                    if j % 5 == 0:
-                        j = 1
-                        out.write("cdown\n")
-                        out.write("cup\n")
-
-            if rgb[1] != current_rgb[1]:
-                # Move green slider
-                while colour_cursor[0] != 1:
-                    if colour_cursor[0] == 0:
-                        out.write("cdown\n")
-                        colour_cursor[0] = 1
-                    else:
-                        out.write("cup\n")
-                        if colour_cursor[2] != 0:
-                            colour_cursor[2] -= 1
-                        else:
-                            colour_cursor[0] -= 1
-
-                j = 1
-                while current_rgb[1] > rgb[1]:
-                    out.write("cleft\n")
-                    current_rgb[1] -= 1
-                    j += 1
-                    if j % 5 == 0:
-                        j = 1
-                        out.write("cdown\n")
-                        out.write("cup\n")
-
-                j = 1
-                while current_rgb[1] < rgb[1]:
-                    out.write("cright\n")
-                    current_rgb[1] += 1
-                    j += 1
-                    if j % 4 == 0:
-                        j = 1
-                        out.write("cdown\n")
-                        out.write("cup\n")
-
-            if rgb[2] != current_rgb[2]:
-                # Move blue slider
-                while colour_cursor[0] != 2:
-                    if colour_cursor[0] == 0:
-                        out.write("cdown\n")
-                        out.write("cdown\n")
-                        colour_cursor[0] = 2
-                    elif colour_cursor[0] == 1:
-                        out.write("cdown\n")
-                        colour_cursor[0] = 2
-                    else:
-                        out.write("cup\n")
-                        if colour_cursor[2] != 0:
-                            colour_cursor[2] -= 1
-                        else:
-                            colour_cursor[0] -= 1
-
-                j = 1
-                while current_rgb[2] > rgb[2]:
-                    out.write("cleft\n")
-                    current_rgb[2] -= 1
-                    j += 1
-                    if j % 4 == 0:
-                        j = 1
-                        out.write("cup\n")
-                        out.write("cdown\n")
-
-                j = 1
-                while current_rgb[2] < rgb[2]:
-                    out.write("cright\n")
-                    current_rgb[2] += 1
-                    j += 1
-                    if j % 4 == 0:
-                        j = 1
-                        out.write("cup\n")
-                        out.write("cdown\n")
+            # Adjust RGB sliders
+            move_rgb_slider(colour_cursor, 0, current_rgb, rgb, out)
+            move_rgb_slider(colour_cursor, 1, current_rgb, rgb, out)
+            move_rgb_slider(colour_cursor, 2, current_rgb, rgb, out)
 
             # Move to target cursor
             target_cursor = (x, y)
-
-            while target_cursor[0] != cursor[0]:
-                if target_cursor[0] < cursor[0]:
-                    out.write("left\n")
-                    cursor[0] -= 1
-                else:
-                    out.write("right\n")
-                    cursor[0] += 1
-
-            while target_cursor[1] != cursor[1]:
-                if target_cursor[1] < cursor[1]:
-                    out.write("up\n")
-                    cursor[1] -= 1
-                else:
-                    out.write("down\n")
-                    cursor[1] += 1
+            move_cursor(cursor, target_cursor, out)
 
             # Draw
             out.write("a\n")
@@ -353,64 +286,38 @@ def decimal_range(start, stop, increment):
         yield start
         start += increment
 
-lowestLines = 999999999
-bestBias = 1.0
+
+def determine_best_bias():
+    global colour_bias
+
+    initialise_colours()
+
+    lowestLines = 999999999
+    bestBias = 1.0
+
+    for i in decimal_range(0.9, 3.0, 0.01):
+        colour_bias = i
+
+        initialise_colours()
+
+        build_pixels()
+        build_sequence()
+        f = open("out.txt", "r", encoding="utf-8").readlines()
+        print("Bias " + str(i) + ": " + str(len(f)))
+
+        if len(f) < lowestLines:
+            lowestLines = len(f)
+            bestBias = i
+
+    print("Best bias is " + str(bestBias) + " at " + str(lowestLines) + " steps")
+
+    return bestBias
 
 
-for i in decimal_range(0.9, 3.0, 0.01):
-    colour_bias = i
+# colour_bias = determine_best_bias()
+colour_bias = 1.7
 
-    pre_defined_colours = {
-        "blank": (0, 0, 0, []),
-        "red": (232, 24, 32, []),
-        "orange": (240, 144, 24, []),
-        "yellow": (248, 240, 0, []),
-        "light_green": (136, 192, 56, []),
-        "green": (56, 176, 72, []),
-        "light_blue": (0, 168, 232, []),
-        "blue": (0, 80, 160, []),
-        "purple": (96, 40, 144, []),
-        "pink": (232, 0, 136, []),
-        "dark_red": (152, 8, 8, []),
-        "gold": (128, 120, 0, []),
-        "dark_green": (0, 88, 32, []),
-        "dark_blue": (0, 48, 96, []),
-        "white": (248, 248, 248, []),
-        "grey": (128, 128, 128, []),
-        "black": (0, 0, 0, [])
-    }
-
-    build_pixels()
-    build_sequence()
-    f = open("out.txt", "r", encoding="utf-8").readlines()
-    print("Bias " + str(i) + ": " + str(len(f)))
-
-    if len(f) < lowestLines:
-        lowestLines = len(f)
-        bestBias = i
-
-print("Best bias is " + str(bestBias) + " at " + str(lowestLines) + " steps")
-colour_bias = bestBias
-
-pre_defined_colours = {
-    "blank": (0, 0, 0, []),
-    "red": (232, 24, 32, []),
-    "orange": (240, 144, 24, []),
-    "yellow": (248, 240, 0, []),
-    "light_green": (136, 192, 56, []),
-    "green": (56, 176, 72, []),
-    "light_blue": (0, 168, 232, []),
-    "blue": (0, 80, 160, []),
-    "purple": (96, 40, 144, []),
-    "pink": (232, 0, 136, []),
-    "dark_red": (152, 8, 8, []),
-    "gold": (128, 120, 0, []),
-    "dark_green": (0, 88, 32, []),
-    "dark_blue": (0, 48, 96, []),
-    "white": (248, 248, 248, []),
-    "grey": (128, 128, 128, []),
-    "black": (0, 0, 0, [])
-}
+initialise_colours()
 
 print("Building pixels...")
 build_pixels()
